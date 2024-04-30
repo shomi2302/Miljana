b0de650a0d7a206d92bb246f8c947ce6a391b395
shomi2302 <shoshtar2302@gmail.com>
2024-04-30 17:00:13 +0200

    Updates

---
+++ precache-manifest.7ed48d7038975f5b1d721c52c3d731ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe125d213730d85fda6fc311311b8a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a30cdf11432b9f33697f",
+    "url": "/static/css/main.dec7a4e3.chunk.css"
+  },
+  {
+    "revision": "6085801c5a366a19edc7",
+    "url": "/static/js/2.b2f93a7c.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/static/js/2.b2f93a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30cdf11432b9f33697f",
+    "url": "/static/js/main.134a0b0e.chunk.js"
+  },
+  {
+    "revision": "e3204b267d8d9b038593",
+    "url": "/static/js/runtime-main.49b69cd8.js"
+  },
+  {
+    "revision": "1c39a974f57514d8e158a4a792a606a2",
+    "url": "/static/media/facebook.1c39a974.svg"
+  },
+  {
+    "revision": "97f33902d1ea503117eba12bc3f1b62b",
+    "url": "/static/media/home-img.97f33902.jpg"
+  },
+  {
+    "revision": "93d17e8145c16b59602515d790b32d40",
+    "url": "/static/media/instagram.93d17e81.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chabi/precache-manifest.8f9fa8458393fe526924a89e93109c7e.js"
+  "/precache-manifest.7ed48d7038975f5b1d721c52c3d731ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chabi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
